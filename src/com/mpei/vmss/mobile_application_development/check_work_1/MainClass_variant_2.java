77f29c219047b7fdbf55ff54d0373a631a1bc023
agaperra <agaperra@yandex.ru>
2021-09-20 10:01:12 +0300

    сделаны задания на кр: - вариант 1 задания 1,2 - вариант 2 задание 1,2

--- src/com/mpei/vmss/mobile_application_development/check_work_1/MainClass_variant_2.java
+++ src/com/mpei/vmss/mobile_application_development/check_work_1/MainClass_variant_2.java
@@ -51,7 +51,50 @@ public class MainClass_variant_2 {
         System.out.println( "Длина этой последовательности равна " + newArr_2.length );
 
 
-        // ариант 2 Задание 2
+        // Вариант 2 Задание 2
+        int[] array_3 = {4, 3, 1, 4, 2, 5, 8};
+        System.out.println( "Исходный массив: " + Arrays.toString( array_3 ) );
+        int s0 = 11;
+        int[] newArr_3 = findSumSequence( array_3, s0 );
+        System.out.println( "Последовательность в массиве "
+                + Arrays.toString( array_3 )
+                + ", дающая в сумме число "
+                + s0
+                + ": "
+                + Arrays.toString( newArr_3 ) );
+
+        int[] array_4 = {4, 3, 1, 4, 2, 5, 8};
+        System.out.println( "Исходный массив: " + Arrays.toString( array_4 ) );
+        int s1 = 8;
+        int[] newArr_4 = findSumSequence( array_4, s1 );
+        System.out.println( "Последовательность в массиве "
+                + Arrays.toString( array_4 )
+                + ", дающая в сумме число "
+                + s1
+                + ": "
+                + Arrays.toString( newArr_4 ) );
+
+        int[] array_5 = {4, 3, 1, 4, 2, 5, 8};
+        System.out.println( "Исходный массив: " + Arrays.toString( array_5 ) );
+        int s2 = 7;
+        int[] newArr_5 = findSumSequence( array_5, s2 );
+        System.out.println( "Последовательность в массиве "
+                + Arrays.toString( array_3 )
+                + ", дающая в сумме число "
+                + s2
+                + ": "
+                + Arrays.toString( newArr_5 ) );
+
+        int[] array_6 = {4, 3, 1, 4, 2, 5, 8};
+        System.out.println( "Исходный массив: " + Arrays.toString( array_6 ) );
+        int s3 = 10;
+        int[] newArr_6 = findSumSequence( array_6, s3 );
+        System.out.println( "Последовательность в массиве "
+                + Arrays.toString( array_6 )
+                + ", дающая в сумме число "
+                + s3
+                + ": "
+                + Arrays.toString( newArr_6 ) );
 
 
     }
@@ -71,6 +114,24 @@ public class MainClass_variant_2 {
         return Arrays.copyOfRange( arr, start, end + 1 );
     }
 
+    private static int[] findSumSequence(int[] arr, int s) {
+        int sum = 0;
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                sum += arr[j];
+                count++;
+                if (sum > s) {
+                    sum = 0;
+                    count = 0;
+                    break;
+                } else if (sum == s) {
+                    return Arrays.copyOfRange( arr, j - count + 1, j + 1 );
+                }
+            }
+        }
+        return Arrays.copyOfRange( arr, 0, 0 );
+    }
 
 
 }
